9e491ca494990b98f1b393c692bb95109b3161b8
Justin <89544777+JustinLuft@users.noreply.github.com>
2025-06-20 00:17:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from scrapers.scraper_tradeify import scrape_site_tradeify  # Import the function directly
-from scrapers.scraper_apex import scrape_site_apex  # Import the wrapper function
+from scrapers.scraper_tradeify import scraper_tradeify  # Import the function directly
+from scrapers.scraper_apex import scraper_apex  # Import the wrapper function
 
 def main():
     try:
